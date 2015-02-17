46c50a48931a74b71d7aa5c08fc4dc63374ca9b2
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-16 20:00:40 -0600

    added getJSON function

--- js/flickr.js
+++ js/flickr.js
@@ -5,5 +5,19 @@ $(document).ready(function() {
     $('button').removeClass('selected');
     $(this).addClass('selected');
 
+    var flickerAPI = 'http://api.flicker.com/services/feeds/photos_public.gne?jsoncallback=?';
+    var animal = $(this).text();
+    var flickerOptions = {
+      tags: animal,
+      format: 'json'
+    };
+
+    function displayPhotos(data) {
+
+    }
+
+    $.getJSON(flickerAPI, flickerOptions, displayPhotos);
+
+
   });
 });
