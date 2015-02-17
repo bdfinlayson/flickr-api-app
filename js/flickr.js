18bd6615446a7bc25358e9fa76d434fb25aa5027
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-17 11:08:34 -0600

    added user input field for custom api searchs via flicker api

--- js/flickr.js
+++ js/flickr.js
@@ -1,34 +1,54 @@
 $(document).ready(function() {
+    var flickrAPI = 'http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+
+    var animal;
+
+    var flickrOptions;
+
+//on form button click
+  $('#submitInput').on('click', function(evt) {
+    evt.preventDefault();
+    console.log('click registered');
+    //var $searchButton = $('#submitInput');
+    var userInput = $('input:first').val();
+
+    flickrOptions = {
+     tags: userInput,
+     format: 'json'
+    }
+    $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+  })
+
+//on animal button click
 
   $('button').click(function() {
 
     $('button').removeClass('selected');
     $(this).addClass('selected');
 
-    var flickerAPI = 'http://api.flicker.com/services/feeds/photos_public.gne?jsoncallback=?';
-    var animal = $(this).text();
-    var flickerOptions = {
+    animal = $(this).text();
+    flickrOptions = {
       tags: animal,
       format: 'json'
     };
+    $.getJSON(flickrAPI, flickrOptions, displayPhotos);
+  });
 
-    function displayPhotos(data) {
+function displayPhotos(data) {
 
-      var photoHTML = '<ul>';
-      $.each(data.items, function (i,photo) {
+  var photoHTML = '<ul>';
+  $.each(data.items, function (i,photo) {
 
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href=" '+ photo.link +' " class="image">';
-        photoHTML += '<img src="' + photomedia.m +'" </a></li>;
-
-      });
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
-    }
-
-
-    $.getJSON(flickerAPI, flickerOptions, displayPhotos);
-
+    photoHTML += '<li class="grid-25 tablet-grid-50">';
+    photoHTML += '<a href=" '+ photo.link +' " class="image">';
+    photoHTML += '<img src="' + photo.media.m +'" </a></li>';
 
   });
+  photoHTML += '</ul>';
+  $('#photos').html(photoHTML);
+}
+
+
+
+
 });
